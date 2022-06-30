4fcb8fc7ff7b322f2ed0e8a8d97223267421d214
Ladislav Ondris <ladislav.ondris@seznam.cz>
2022-06-30 20:42:30 +0200

    logging in client

--- client.py
+++ client.py
@@ -1,18 +1,8 @@
 import json
+import logging
 from struct import pack, unpack
 
 import paho.mqtt.client as mqtt
-
-broker = 'eu1.cloud.thethings.network'
-port = 1883
-app_id = 'testv3tg'
-device_id = 'eui-70b3d57ed0050aa7'
-access_key = "NNSXS.UM2CNVLZH662W6JONDEEV5CDEB3RSUGPTEQUFJI.55KSB5VIOIBR3MKKAE34NYEUVH5KIIA5TNQGWBR7DTJ3MVRCCSFQ"
-
-
-def on_connect(mqttc, mosq, obj, rc):
-    print("Connected with result code:" + str(rc))
-    mqttc.subscribe(f'v3/+/devices/{device_id}/up')
 
 
 class Payload:
@@ -29,6 +19,12 @@ class Payload:
                f'latitude={self.latitude}, ' \
                f'longitude={self.longitude}, ' \
                f'vbattery={self.vbattery}, vcapacitor={self.vcapacitor})>'
+
+
+def on_connect(mqttc, mosq, obj, rc):
+    logging.info(f"Connected with result code: {rc}")
+    logging.info(f"Subscribing to device: {device_id}")
+    mqttc.subscribe(f'v3/+/devices/{device_id}/up')
 
 
 def reinterpret_int_as_float(number) -> float:
@@ -51,38 +47,48 @@ def read_payload(decoded_payload_json) -> Payload:
 
 
 def on_message(mqttc, obj, msg):
+    logging.info("Received message. Decoding content...")
+
     x = json.loads(msg.payload.decode('utf-8'))
     uplink_message = x['uplink_message']
     frm_payload = uplink_message['frm_payload']
     decoded_payload = uplink_message['decoded_payload']
 
-    print(x)
-    print(read_payload(decoded_payload))
+    payload: Payload = read_payload(decoded_payload)
 
 
 def on_publish(mosq, obj, mid):
-    print("Publish: mid: " + str(mid))
+    logging.info(f"Published: mid: {mid}")
 
 
 def on_subscribe(mosq, obj, mid, granted_qos):
-    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+    logging.info(f"Subscribed: {mid} {granted_qos}")
 
 
-def on_log(mqttc, obj, level, buf):
-    print("Log: message:" + str(buf))
-    print("Log: userdata:" + str(obj))
+logging.basicConfig(filename='picoballoon.log',
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.INFO)
 
+broker = 'eu1.cloud.thethings.network'
+port = 1883
+app_id = 'testv3tg'
+device_id = 'eui-70b3d57ed0050aa7'
+access_key = "NNSXS.UM2CNVLZH662W6JONDEEV5CDEB3RSUGPTEQUFJI.55KSB5VIOIBR3MKKAE34NYEUVH5KIIA5TNQGWBR7DTJ3MVRCCSFQ"
+
+logging.info(f"Setting up MQTT client: app_id={app_id}, port={port}, broker={broker}")
 
 mqttc = mqtt.Client()
 mqttc.on_connect = on_connect
 mqttc.on_message = on_message
 mqttc.on_publish = on_publish
 mqttc.on_subscribe = on_subscribe
-mqttc.on_log = on_log
 
 mqttc.username_pw_set(app_id, access_key)
 mqttc.connect(broker, port)
 
+logging.info(f"Starting infinite loop, waiting for messages...")
 run = True
 while run:
     mqttc.loop()
