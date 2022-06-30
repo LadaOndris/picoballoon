9ee008522dad0766bf1ea1217c719323387585a7
Ladislav Ondris <ladislav.ondris@seznam.cz>
2022-06-30 20:49:44 +0200

    client logging

--- client.py
+++ client.py
@@ -21,10 +21,13 @@ class Payload:
                f'vbattery={self.vbattery}, vcapacitor={self.vcapacitor})>'
 
 
-def on_connect(mqttc, mosq, obj, rc):
-    logging.info(f"Connected with result code: {rc}")
-    logging.info(f"Subscribing to device: {device_id}")
-    mqttc.subscribe(f'v3/+/devices/{device_id}/up')
+def setup_on_connect_callback(device_id):
+    def on_connect(mqttc, mosq, obj, rc):
+        logging.info(f"Connected with result code: {rc}")
+        logging.info(f"Subscribing to device: {device_id}")
+        mqttc.subscribe(f'v3/+/devices/{device_id}/up')
+
+    return on_connect
 
 
 def reinterpret_int_as_float(number) -> float:
@@ -65,28 +68,25 @@ def on_subscribe(mosq, obj, mid, granted_qos):
     logging.info(f"Subscribed: {mid} {granted_qos}")
 
 
-logging.basicConfig(filename='picoballoon.log',
+logging.basicConfig(filename='picoballoon_client.log',
                     filemode='a',
                     format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
                     datefmt='%H:%M:%S',
                     level=logging.INFO)
 
-broker = 'eu1.cloud.thethings.network'
-port = 1883
-app_id = 'testv3tg'
-device_id = 'eui-70b3d57ed0050aa7'
-access_key = "NNSXS.UM2CNVLZH662W6JONDEEV5CDEB3RSUGPTEQUFJI.55KSB5VIOIBR3MKKAE34NYEUVH5KIIA5TNQGWBR7DTJ3MVRCCSFQ"
+with open("config.json", "r") as config_file:
+    config = json.load(config_file)['client']
 
-logging.info(f"Setting up MQTT client: app_id={app_id}, port={port}, broker={broker}")
+logging.info(f'Setting up MQTT client: app_id={config["app_id"]}, broker={config["broker"]}, port={config["port"]}')
 
 mqttc = mqtt.Client()
-mqttc.on_connect = on_connect
+mqttc.on_connect = setup_on_connect_callback(config["device_id"])
 mqttc.on_message = on_message
 mqttc.on_publish = on_publish
 mqttc.on_subscribe = on_subscribe
 
-mqttc.username_pw_set(app_id, access_key)
-mqttc.connect(broker, port)
+mqttc.username_pw_set(config["app_id"], config["access_key"])
+mqttc.connect(config["broker"], config["port"])
 
 logging.info(f"Starting infinite loop, waiting for messages...")
 run = True
